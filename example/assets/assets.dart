ee4ab44cb0109dac6cf28d07215e856a5e11fe71
TheBoringDude <iamcoderx@gmail.com>
2022-02-05 00:30:06 +0800

    Organize examples by each endpoint and file.

---
+++ example/assets/assets.dart
@@ -0,0 +1,8 @@
+import 'package:atomicassets/atomicassets.dart';
+
+void main() async {
+  var aa = AtomicAssets("https://wax.api.atomicassets.io");
+
+  var data = await aa.getAssets();
+  print(data.data?[0]);
+}
--- example/assets/id.dart
+++ example/assets/id.dart
@@ -3,9 +3,6 @@ import 'package:atomicassets/atomicassets.dart';
 void main() async {
   var aa = AtomicAssets("https://wax.api.atomicassets.io");
 
-  var data = await aa.getAssets();
-  print(data.data?[0]);
-
   var nft = await aa.getAssetID("1099596594113");
   print(nft.data);
 }
